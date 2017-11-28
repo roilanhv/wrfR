fb6affeedcd3b4079b7799119e68a0de17c74abe
Roilan Hdez Valdes <roylanhv@gmail.com>
2017-11-28 09:17:48 -0200

    Identation code
    Can't see if parallelization would be better in this function

--- R/vert.interp.R
+++ R/vert.interp.R
@@ -9,16 +9,26 @@
 #'  @author I. Lopez-Coto, 2014 (israel.lopez@@dfa.uhu.es / inl@@nist.gov)
 #'  @export
 
-vert.interp <- function(subset,zabg,zout)
+vert.interp <- function(subset, zabg, zout)
 {
+  # require(parallel)
   
-  tempout <- array(0,dim=dim(zabg))
-  for (n in 1:dim(subset)[1])
-  { 
-    for (m in 1:dim(subset)[2])
-    { 
-      tempout[n,m,] <- approx(zabg[n,m,],subset[n,m,1:dim(zabg)[3]], xout=zout,rule = 2)$y
+  if (!is.null(zout)) {
+    tempout <-
+      array(0, dim = c(dim(zabg)[1], dim(zabg)[2], length(zout)))
+    
+    for (n in 1:dim(subset)[1])
+    {
+      for (m in 1:dim(subset)[2])
+      {
+        tempout[n, m, ] <-
+          approx(zabg[n, m, ], subset[n, m, 1:dim(zabg)[3]], xout = zout, rule = 2)$y
+      }
+      return(tempout)
     }
+  } else {
+    return(subset)
   }
-  return(tempout)
 }
+
+
