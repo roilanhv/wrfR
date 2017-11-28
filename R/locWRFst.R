1a28b89f2ad95fc8b583657bd1f3e98299989b43
Roilan Hdez Valdes <roylanhv@gmail.com>
2017-11-28 09:15:31 -0200

    Less lines in locWRFst function

--- R/locWRFst.R
+++ R/locWRFst.R
@@ -9,50 +9,36 @@
 #'  @author I. Lopez-Coto, 2013 (israel.lopez@@dfa.uhu.es / inl@@nist.gov)
 #'  @export
 
-locWRFst <- function(fileWRF,st.data)
+
+locWRFst <- function(fileWRF, st.data){
   
-{
-  
-require(raster)
+  require(raster)
   
   ## Get the WRF grid
   
   WRFgrid <- getGRID(fileWRF)
-  for (i in 1:length(names(WRFgrid))){
-    assign(names(WRFgrid)[[i]],WRFgrid[[i]]) 
+  for (i in 1:length(names(WRFgrid))) {
+    assign(names(WRFgrid)[[i]], WRFgrid[[i]])
   }
   
-  ## Localize stations indexes 
-  
-  st.loc <- matrix(0,nrow=dim(st.data)[1],ncol=6)
-  st.names <- matrix(0,nrow=dim(st.data)[1],ncol=1)
-  
-  st.loc[,2:4] <- cbind(st.data$Lat,st.data$Lon,st.data$AGL)
-  st.loc[,1] <- st.data$Name
-  
+  ## Localize stations indexes
   
   for (st in 1:dim(st.data)[1])
   {
-    
     # Get the station location
-    dist <- pointDistance(c(st.data$Lon[st],st.data$Lat[st]),cbind(as.vector(lon_WRF),as.vector(lat_WRF)),longlat=TRUE,r=6370*1e3) # Uses WRF earth radius
-    dist <- matrix(dist,ncol=ncol(lon_WRF),nrow=nrow(lon_WRF))
-    stloc <- which(dist==min(dist),arr.ind = TRUE)
+    dist <-
+      pointDistance(
+        c(st.data$lon[st], st.data$lat[st]),
+        cbind(as.vector(lon_WRF), as.vector(lat_WRF)),
+        longlat = TRUE,
+        r = 6370 * 1e3
+      ) # Uses WRF earth radius
+    dist <- matrix(dist, ncol = ncol(lon_WRF), nrow = nrow(lon_WRF))
+    stloc <- which(dist == min(dist), arr.ind = TRUE)
     
-    lat_indx<-stloc[2]
-    lon_indx<-stloc[1]
-    st.loc[st,5:6]<-c(lat_indx,lon_indx)
+    st.data$lat_indx <- stloc[2]
+    st.data$lon_indx <- stloc[1]
   }
   
-  dimnames(st.loc)[[2]]<-c('name','lat','lon','elev','lat_indx','lon_indx')
-  
-  st.loc<-data.frame(st.loc,stringsAsFactors=F)
-
-  st.loc$lat<-as.numeric(st.loc$lat)
-  st.loc$lon<-as.numeric(st.loc$lon)
-  st.loc$elev<-as.numeric(st.loc$elev)
-  st.loc$lat_indx<-as.numeric(st.loc$lat_indx)
-  st.loc$lon_indx<-as.numeric(st.loc$lon_indx)
-  
-  return(st.loc)
+  return(st.data)
 }
